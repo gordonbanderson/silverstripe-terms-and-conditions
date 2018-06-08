9540f3b5081e1f29b7240e669dd0ab3b643d0eb5
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-06-08 16:28:48 +0700

    ENHANCEMENT: Added more fields, get ip method

--- src/Model/TOCSignature.php
+++ src/Model/TOCSignature.php
@@ -12,11 +12,34 @@ class TOCSignature extends DataObject
     private static $table_name = 'TOCSignature';
 
     private static $db = [
-        'Title' => 'Varchar(255)'
+        'Title' => 'Varchar(255)',
+
+        // if the member is not available
+        'Email' => 'Varchar(255)',
+
+        // IP address
+        'Address' => 'Varchar(255)',
+
+        // for likes of 'event regirstation'
+        'Note' => 'Varchar(255)',
+
+        // the typed in signature
+        'Signature' => 'Varchar(255)'
     ];
 
     private static $has_one = [
         'Member' => Member::class,
         'TermsAndConditionsFile' => File::class
     ];
+
+    public static function getIPAddress()
+    {
+        // see https://stackoverflow.com/questions/15699101/get-the-client-ip-address-using-php
+        return getenv('HTTP_CLIENT_IP')?:
+            getenv('HTTP_X_FORWARDED_FOR')?:
+                getenv('HTTP_X_FORWARDED')?:
+                    getenv('HTTP_FORWARDED_FOR')?:
+                        getenv('HTTP_FORWARDED')?:
+                            getenv('REMOTE_ADDR');
+    }
 }
